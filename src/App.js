213dd59e745ce2ca9d3cbe82477e7c73c1294400
Jonathan C MacMillan <jmacmill@gmail.com>
2023-01-12 16:35:08 +1000

    Import MUI components. Build template Gallery page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import ButtonAppBar from './components/Container';
 
 function App() {
-  const [something, setSomething] = useState(null)
+  const [test, setTest] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-  const checkSomething = async () => {
+  const runTest = async () => {
     try {
-      const response = await fetch('https://localhost:5000')
+      const response = await fetch('http://localhost:5000/data.json')
       const data = await response.json()
-      setSomething(data)
+      setTest(data)
     } catch (err) {
       setError('Something went wrong!')
     }
   }
-  checkSomething()
+  runTest()
 }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>{something}</p>
+      <ButtonAppBar/>
+      <p>{test}</p>
     </div>
   );
 }
