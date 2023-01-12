4235a0653c60599084a2302f7a0be1762206043b
Jonathan C MacMillan <jmacmill@gmail.com>
2023-01-12 16:41:38 +1000

    Update to do items

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { css } from "@emotion/react"
-import Button from '@mui/material/Button';
 import App from "./App.js";
-
-// const App = () => {
-//   return (
-//     <Button
-//     css={css`
-//              margin-left: 10px;
-//         `}
-//         variant="contained" color="primary">
-//       Orange Digital Test
-//     </Button>
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
